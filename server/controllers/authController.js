d1f70c44a999f066073a6e7718675bff60197e6c
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-04-14 19:08:31 -0700

    Started sign up logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,54 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 // Required model
 const User = require('../models/userModel');
 
 // Handle signup POST to create new user
-exports.sign_up_post = asyncHandler(async (req, res, next) => {
-  res.json('sign up');
-});
+exports.sign_up_post = [
+  body('first_name')
+    .trim()
+    .isLength({ min: 3, max: 120 })
+    .withMessage('First Name must contain at least 3 characters')
+    .escape(),
+  body('last_name')
+    .trim()
+    .isLength({ min: 3, max: 120 })
+    .withMessage('Last Name must contain at least 3 characters')
+    .escape(),
+  body('email')
+    .trim()
+    .isLength({ min: 3, max: 120 })
+    .withMessage('Email must contain at least 3 characters')
+    .escape(),
+  body('username')
+    .trim()
+    .isLength({ min: 3, max: 120 })
+    .withMessage('Username must contain at least 3 characters')
+    .escape(),
+  body('password')
+    .trim()
+    .isLength({ min: 3, max: 120 })
+    .withMessage('Password must contain at least 3 characters')
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    if(!errors.isEmpty()) {
+      res.json({
+        user,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await user.save();
+    }
+})];
