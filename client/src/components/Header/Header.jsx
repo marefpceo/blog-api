48cbd9d36874196f8afb8279c31dde8abe108dd7
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-04-19 08:53:03 -0700

    Created Nav component with placeholder links

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import Nav from '../Nav/Nav';
 import './Header.css';
 
 function Header() {
   return (
-    <header className='rounded-b-lg shadow-md'>
+    <header className='rounded-b-lg shadow-md fixed left-0 top-0 right-0'>
       <h1>BlogAPI</h1>
+      <Nav />
     </header>
   );
 }
---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import '../Nav/Nav.css';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>Articles</li>
+        <li>About</li>
+        <li>Subscribe</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
