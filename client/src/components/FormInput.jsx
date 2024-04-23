c89fc6462db204f341529e665f834a2a2a0c0d66
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-04-23 10:25:35 -0700

    Signup form is about 80 percent complete
    Completed form formatting. Ready for final styling.
    Text input field state logic completed.
    Cancel button logic completed.
    
    Submit button logic needs to be completed

--- client/src/components/FormInput.jsx
+++ client/src/components/FormInput.jsx
@@ -1,10 +1,30 @@
-function TextField({ htmlFor, type, name, id, fieldname }) {
+function FormInput({
+  htmlFor,
+  type,
+  name,
+  id,
+  fieldname,
+  handleInputChange,
+  value,
+}) {
   return (
-    <label htmlFor={htmlFor}>
+    <label
+      className='flex flex-col items-start leading-normal'
+      htmlFor={htmlFor}
+    >
       {fieldname}
-      <input type={type} name={name} id={id} required />
+      <input
+        className='mt-1 min-w-96 rounded-md p-1 shadow-inner shadow-slate-400 focus:border-2 
+          focus:border-green-500 focus:outline-none'
+        type={type}
+        name={name}
+        id={id}
+        onChange={handleInputChange}
+        value={value}
+        required
+      />
     </label>
   );
 }
 
-export default TextField;
+export default FormInput;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,56 +1,90 @@
-import TextField from '../components/FormInput';
+import FormInput from '../components/FormInput';
 import Button from '../components/Button';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Signup() {
+  const [userInput, setUserInput] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    username: '',
+    password: '',
+    confirm_password: '',
+  });
+
+  function handleInputChange(e) {
+    const value = e.target.value;
+    setUserInput({
+      ...userInput,
+      [e.target.name]: value,
+    });
+  }
+
   return (
-    <section className='signup'>
-      <form method='post' className='form mx-auto flex max-w-xl flex-col gap-8'>
-        <TextField
+    <section className='signup mb-40 mt-5'>
+      <form
+        method='post'
+        className='form mx-auto flex max-w-xl flex-col items-center gap-8 rounded-md border py-16 shadow-xl shadow-slate-300'
+      >
+        <FormInput
           htmlFor={'first_name'}
           type={'text'}
           name={'first_name'}
           id={'first_name'}
-          fieldname={'First Name:'}
+          fieldname={'First Name'}
+          handleInputChange={handleInputChange}
+          value={userInput.first_name}
         />
 
-        <TextField
+        <FormInput
           htmlFor={'last_name'}
           type={'text'}
           name={'last_name'}
           id={'last_name'}
-          fieldname={'Last Name:'}
+          fieldname={'Last Name'}
+          handleInputChange={handleInputChange}
+          value={userInput.last_name}
         />
 
-        <TextField
+        <FormInput
           htmlFor={'email'}
           type={'text'}
           name={'email'}
           id={'email'}
-          fieldname={'Email:'}
+          fieldname={'Email'}
+          handleInputChange={handleInputChange}
+          value={userInput.email}
         />
 
-        <TextField
+        <FormInput
           htmlFor={'username'}
           type={'text'}
           name={'username'}
           id={'username'}
-          fieldname={'Username:'}
+          fieldname={'Username'}
+          handleInputChange={handleInputChange}
+          value={userInput.username}
         />
 
-        <TextField
+        <FormInput
           htmlFor={'password'}
           type={'text'}
           name={'password'}
           id={'password'}
-          fieldname={'Password:'}
+          fieldname={'Password'}
+          handleInputChange={handleInputChange}
+          value={userInput.password}
         />
 
-        <TextField
+        <FormInput
           htmlFor={'confirm_password'}
           type={'text'}
           name={'confirm_password'}
           id={'confirm_password'}
-          fieldname={'Confirm_Password:'}
+          fieldname={'Confirm Password'}
+          handleInputChange={handleInputChange}
+          value={userInput.confirm_password}
         />
 
         <fieldset className='flex justify-center gap-8'>
@@ -71,22 +105,24 @@ function Signup() {
             }}
           />
 
-          <Button
-            className={'shadow-md'}
-            text={'Cancel'}
-            type={'button'}
-            style={{
-              backgroundColor: '#f3f3f3',
-              color: 'black',
-              borderRadius: '8px',
-              padding: '0.3em 1.2em',
-              height: '36px',
-              fontSize: '1em',
-              fontFamily: 'inherit',
-              cursor: 'pointer',
-              transition: 'border-color 0.25',
-            }}
-          />
+          <Link to='/'>
+            <Button
+              className={'shadow-md'}
+              text={'Cancel'}
+              type={'button'}
+              style={{
+                backgroundColor: '#f3f3f3',
+                color: 'black',
+                borderRadius: '8px',
+                padding: '0.3em 1.2em',
+                height: '36px',
+                fontSize: '1em',
+                fontFamily: 'inherit',
+                cursor: 'pointer',
+                transition: 'border-color 0.25',
+              }}
+            />
+          </Link>
         </fieldset>
       </form>
     </section>
