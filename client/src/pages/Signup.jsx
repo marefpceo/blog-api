7bb60043ec863923b0b1160da040d593a8d7e620
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-04-22 18:44:13 -0700

    Updating local with remote

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,15 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
 function Signup() {
-  return <></>;
+  return (
+    <section className='signup'>
+      <form method='post'>
+        <label htmlFor='first_name'>
+          {' '}
+          First Name:
+          <input type='text' name='first_name' id='first_name' />
+        </label>
+      </form>
+    </section>
+  );
 }
 
 export default Signup;
