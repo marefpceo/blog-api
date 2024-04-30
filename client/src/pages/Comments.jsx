8ed88e810787bba962a9588a01e27debcce0a130
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-04-29 19:11:46 -0700

    Added Leave Comment Button to Comments page

--- client/src/pages/Comments.jsx
+++ client/src/pages/Comments.jsx
@@ -2,9 +2,14 @@ import { useOutletContext } from 'react-router-dom';
 import CommentCard from '../components/CommentCard';
 import blogImgPlaceholder from '../assets/images/blog-img-placeholder.png';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import Button from '../components/Button';
 
 function Comments() {
+  const { id } = useParams();
   const { articleComments, selectedArticle } = useOutletContext({});
+  const { isAuthenticated } = useOutletContext();
   const navigate = useNavigate();
 
   return (
@@ -32,8 +37,37 @@ function Comments() {
           </span>
         </div>
       </div>
+
       <section className='comments flex-1'>
-        <h1>Comments</h1>
+        <div className='comments-header'>
+          <h1>Comments</h1>
+          <Link
+            to={
+              isAuthenticated === false
+                ? '/signup'
+                : `/article/${id}/leave_comment`
+            }
+          >
+            <Button
+              className={'shadow-md'}
+              type={'button'}
+              text={
+                isAuthenticated === false ? 'Create Account' : 'Leave a Comment'
+              }
+              style={{
+                backgroundColor: 'green',
+                color: 'black',
+                borderRadius: '8px',
+                padding: '0.3em 1.2em',
+                height: '36px',
+                fontSize: '1em',
+                fontFamily: 'inherit',
+                cursor: 'pointer',
+                transition: 'border-color 0.25',
+              }}
+            />
+          </Link>
+        </div>
         <div className='comments-div mt-16'>
           {articleComments &&
             articleComments.map((comment) => (
