17678fe7e401076cefdc06ccea80b191dda53d4c
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-05-12 08:44:11 -0700

    Created AsideNav menu with no Links yet

--- admin/src/components/AsideNav.jsx
+++ admin/src/components/AsideNav.jsx
@@ -7,39 +7,39 @@ function AsideNav() {
   const [showMenu, setShowMenu] = useState(false);
 
   function handleClick() {
-    if (showMenu === false) {
-      setShowMenu(true);
-    } else {
-      setShowMenu(false);
-    }
+    showMenu === false ? setShowMenu(true) : setShowMenu(false);
   }
 
   return (
     <nav className='w-full text-xl'>
-      <ul>
+      <ul className='space-y-4'>
         <li>Dashboard</li>
-        <li
-          onClick={handleClick}
-          className='flex cursor-pointer items-center justify-between'
-        >
-          Articles
-          {showMenu === false ? (
-            <FontAwesomeIcon
-              icon={faSortUp}
-              size='xl'
-              style={{ color: '#ea7317' }}
-            />
-          ) : (
-            <FontAwesomeIcon
-              icon={faSortDown}
-              size='xl'
-              style={{ color: '#443850' }}
-            />
-          )}
+        <li className='flex cursor-pointer flex-col items-center justify-between'>
+          <div
+            onClick={handleClick}
+            className='relative flex w-full items-center justify-between'
+          >
+            <span>Articles</span>
+            {showMenu === false ? (
+              <FontAwesomeIcon
+                icon={faSortUp}
+                size='xl'
+                style={{ color: '#ea7317' }}
+                className='absolute right-1 top-1'
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faSortDown}
+                size='xl'
+                style={{ color: '#443850' }}
+                className='absolute bottom-1 right-1'
+              />
+            )}
+          </div>
           {showMenu === false ? (
             ''
           ) : (
-            <ul>
+            <ul className='-ml-20 space-y-2'>
               <li>All</li>
               <li>Published</li>
               <li>UnPublished</li>
