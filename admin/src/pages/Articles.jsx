4359c953ae59d4e16fb8cd4c1d1ca0d1fccc18dc
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-05-13 16:04:04 -0700

    Working on All Articles

--- admin/src/pages/Articles.jsx
+++ admin/src/pages/Articles.jsx
@@ -1,9 +1,66 @@
 import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Articles({ className }) {
+  const [allArticles, setAllArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    async function getArticles() {
+      try {
+        const response = await fetch('http://localhost:3000/admin/articles', {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        if (!response.ok) {
+          const error = new Error();
+          if (response.status === 500) {
+            error.message = 'Internal Server Error';
+            error.status = 500;
+            throw error;
+          }
+          if (response.status === 404) {
+            error.message = 'Page Not Found';
+            error.status = 404;
+            throw error;
+          }
+          error.message = 'API Connection Error';
+          error.status = 500;
+          throw error;
+        } else {
+          let responseData = await response.json();
+          setAllArticles(responseData.articlesList);
+          console.log(responseData);
+        }
+      } catch (error) {
+        console.error(error.message);
+        setAllArticles(null);
+        if (error instanceof TypeError) {
+          return navigate('*', {
+            state: { status: 503, statusMessage: 'Service Unavailable' },
+          });
+        } else {
+          return navigate('*', {
+            state: { status: error.status, statusMessage: error.message },
+          });
+        }
+      } finally {
+        setTimeout(setLoading(false), 3000);
+      }
+    }
+    getArticles();
+  }, []);
+
   return (
     <section className={className}>
-      <Outlet />
+      <Outlet
+        context={{
+          allArticles,
+        }}
+      />
     </section>
   );
 }
--- admin/src/sections/All.jsx
+++ admin/src/sections/All.jsx
@@ -1,5 +1,21 @@
+import { useOutletContext } from 'react-router-dom';
+
 function All() {
-  return <section>All articles</section>;
+  const { allArticles } = useOutletContext([]);
+
+  return (
+    <>
+      <h1 className='text-4xl text-cust-silver'>All Articles</h1>
+
+      <div className='list-div'>
+        <div>
+          {allArticles.map((article) => (
+            <h2 key={article._id}>{article.article_title}</h2>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default All;
