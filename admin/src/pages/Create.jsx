fbe4aabfa980a0e218824a7482aa60854e48d573
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-05-13 20:33:17 -0700

    Added TinyMCE to create page

--- admin/src/pages/Create.jsx
+++ admin/src/pages/Create.jsx
@@ -1,5 +1,56 @@
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
 function Create() {
-  return <section>Create Article</section>;
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
+  return (
+    <>
+      <h1>Create Article</h1>
+      <Editor
+        tinymceScriptSrc='/tinymce/tinymce.min.js'
+        licenseKey='your-license-key'
+        onInit={(_evt, editor) => (editorRef.current = editor)}
+        initialValue='<p>This is the initial content of the editor.</p>'
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'preview',
+            'help',
+            'wordcount',
+          ],
+          toolbar:
+            'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </>
+  );
 }
 
 export default Create;
