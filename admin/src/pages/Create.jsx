f28b0ba2a5bdf6a134d5a95206aac758946852c3
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-05-17 17:34:24 -0700

    Removed tinymce to install lexical

--- admin/src/pages/Create.jsx
+++ admin/src/pages/Create.jsx
@@ -1,60 +1,23 @@
-import { useRef } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import imgPlaceholder from '../assets/images/blog-img-placeholder.png';
 
 function Create() {
-  const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
-
   return (
     <>
-      <h1 className='text-4xl text-cust-silver'>Create Article</h1>
-      <form className='mt-12 flex h-3/4 justify-center'>
-        <Editor
-          tinymceScriptSrc='/tinymce/tinymce.min.js'
-          licenseKey='your-license-key'
-          onInit={(_evt, editor) => (editorRef.current = editor)}
-          initialValue='<p>This is the initial content of the editor.</p>'
-          init={{
-            selector: 'textarea',
-            skin_url: '/src/assets/skins/',
-            height: 600,
-            width: '80%',
-            menubar: false,
-            resize: true,
-            plugins: [
-              'advlist',
-              'autolink',
-              'lists',
-              'link',
-              'image',
-              'charmap',
-              'anchor',
-              'searchreplace',
-              'visualblocks',
-              'code',
-              'fullscreen',
-              'insertdatetime',
-              'media',
-              'table',
-              'preview',
-              'help',
-              'wordcount',
-            ],
-            toolbar:
-              'undo redo | blocks | ' +
-              'bold italic forecolor | alignleft aligncenter ' +
-              'alignright alignjustify | bullist numlist outdent indent | ' +
-              'removeformat | help',
-            content_style:
-              'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-          }}
-        />
-      </form>
-      <button onClick={log}>Log editor content</button>
+      <h1 id='title' className='title text-4xl text-cust-silver'>
+        Create Article
+      </h1>
+      <article className='mx-auto mt-12 flex h-3/4 w-4/6 flex-col justify-start bg-slate-100 text-black'>
+        <div className='main-image mb-16 h-56 w-full'>
+          <img
+            src={imgPlaceholder}
+            alt='Main image placeholder'
+            className='mx-auto h-full w-2/3'
+          />
+        </div>
+        <div>
+          <h2 id='title'>Article Title</h2>
+        </div>
+      </article>
     </>
   );
 }
