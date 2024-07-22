40af9c9502bfec58c76057d7ba58f6dd31965e8f
marefpceo <marefpceo@gmail.com>
2024-07-22 16:17:52 -0700

    Updating local with remote
    Need to complete ErrorPage

--- admin/src/pages/ErrorPage.jsx
+++ admin/src/pages/ErrorPage.jsx
@@ -1,8 +1,19 @@
+import { useRouteError } from 'react-router-dom';
+
 function ErrorPage() {
+  const error = useRouteError();
+
+  console.log(error);
   return (
-    <>
-      <h1>Error Page</h1>
-    </>
+    <div className='m-0 grid h-screen w-screen grid-cols-layout grid-rows-layout p-0'>
+      {error === null ? 
+       ( <div>
+         <p>404 Error</p>
+          <p>Page Not Found</p>
+       </div>) : 
+        ('')
+      }
+    </div>
   );
 }
 
