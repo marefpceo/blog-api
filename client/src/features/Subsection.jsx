e0008d5e1d985adea2abd1fad7feef858a6f37f9
marefpceo <marefpceo@gmail.com>
2024-10-14 16:06:27 -0400

    Changed _id to id on Subsection Card component

--- client/src/features/Subsection.jsx
+++ client/src/features/Subsection.jsx
@@ -10,7 +10,7 @@ function Subsection({ className, title, articles }) {
         {articles &&
           articles.map((article) => (
             <Card
-              key={article._id}
+              key={article.id}
               article={article}
               className={
                 'rounded-sm border border-cust-english-violet/10 p-4 shadow-lg shadow-cust-slate-gray hover:shadow-lg hover:shadow-cust-pumpkin'
